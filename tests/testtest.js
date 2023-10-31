69dce09e6a503b1da10b4d00f7c2916b21f42cfd
ZLY <953472069@qq.com>
2023-10-31 10:41:33 +0800

    test update

--- tests/testtest.js
+++ tests/testtest.js
@@ -1,37 +1,25 @@
-// MyComponent.spec.js
-
-import { shallowMount } from '@vue/test-utils';
-import MyComponent from '@/components/MyComponent.vue';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import { describe, it, expect } from '@jest/globals';
+import { jest, describe, it, expect } from '@jest/globals';
+import getSpt from '@/src/api/shoppingCart.ts'
 
 describe('MyComponent', () => {
-  it('sends a request to the backend on button click', async () => {
-    const mock = new MockAdapter(axios);
-    const wrapper = shallowMount(MyComponent);
-
-    // 模拟HTTP请求的期望URL和响应
-    const url = 'http://localhost:9003/food/serarchNm'; // 后端API的端点
-    const responseData = { message: 'Success' };
-
-    // 模拟按钮点击
-    await wrapper.find('button').trigger('click');
-
-    // 断言HTTP请求是否正确发送
-    mock.onGet(url).reply(200, responseData);
-    await wrapper.vm.$nextTick(); // 等待Vue更新
-    expect(mock.history.post.length).toBe(1);
-
-    // 断言请求的数据是否正确
-    const request = mock.history.post[0];
-    expect(request.url).toBe(url);
-    // expect(JSON.parse(request.data)).toEqual({ /* 请求的数据 */ });
-
-    // 断言响应是否正确
-    expect(wrapper.text()).toContain(responseData.message);
-
-    // 清除请求模拟
-    mock.restore();
-  });
+    it('should fetch data from the server', async () => {
+        const userId = 123; // 用于测试的用户ID
+    
+        // 模拟axios请求
+        const mockAxios = {
+          get: jest.fn(() => Promise.resolve({ data: { content: ['item1', 'item2'] } })),
+        };
+    
+        // 使用mockAxios作为axios的替代
+        jest.mock('axios', () => mockAxios);
+    
+        // 调用函数并等待结果
+        const result = await getSpt(userId);
+    
+        // 验证是否成功调用axios.get
+        expect(mockAxios.get).toHaveBeenCalledWith(`/api/spt?userId=${userId}`);
+    
+        // // 验证返回的数据是否正确
+        // expect(result).toEqual(['item1', 'item2']);
+      });
 });
